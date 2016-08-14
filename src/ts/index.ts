dee564df05590bba056f14c4353c514345467027
Marcos Hernández <marcoslhc@gmail.com>
2016-08-14 19:59:25 -0400

    Add index and remove unused script

--- src/ts/index.ts
+++
@@ -1,6 +0,0 @@
-function parse(myStr: String) {
-    return myStr
-}
-let uri = 'https://www.typescriptlang.org/docs/handbook/integrating-with-build-tools.html';
-let urlObject = parse(uri);
-export default urlObject;
