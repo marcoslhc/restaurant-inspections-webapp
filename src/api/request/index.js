4405f33174b15c316466fb136ecd84a130740218
Marcos Hernández <marcoslhc@gmail.com>
2017-02-18 11:46:52 -0500

    Decouple Http Library from actual request instance creation

--- src/api/request/index.js
+++ src/api/request/index.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 // Host for restaurant-inspections-api
 const host                  = 'http://45.55.191.140/';
 
-// String a, Promise b ~> (a) => b
-const createRequestInstance = baseURL  => axios.create({ baseURL });
+// Axios a, String b, Promise c ~> (a) => (b) => c
+const createRequestInstance = creator => baseURL  => creator({ baseURL });
 
 // Object a ~> (a) => a
 const getResponseData       = response => response.data.data;
@@ -13,7 +13,7 @@ const getResponseData       = response => response.data.data;
 const handleResponseError   = error    => Promise.reject(error);
 
 // Axios instance for the restaurant-inspections-api
-const instance              = createRequestInstance(host);
+const instance              = createRequestInstance(axios.create.bind(axios))(host);
 
 // Removes the wrapper around the actual data
 // It would be handy to allow plug this in or not
