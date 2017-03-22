ccc528260ae76dcc70168ec2de091c25250f0c92
Marcos Hernández <marcoslhc@gmail.com>
2017-03-22 18:09:42 -0400

    Remove "funkadelic" style from this file too

--- src/api/businesses/index.js
+++ src/api/businesses/index.js
@@ -1,25 +1,33 @@
 import request from '../request';
-import { map } from '../../utils';
+import {
+  map
+} from '../../utils';
 
 // Endpoint for all businesses
-const endpoint        = 'businesses';
+const endpoint = 'businesses';
 
 // Object a ~> (a) => a
-const mapBusinessData = business => ({ countyId      : business.county_number,
-                                       address       : business.location_address,
-                                       licenseType   : business.license_type_code,
-                                       name          : business.business_name,
-                                       license       : business.license_number,
-                                       zipCode       : business.location_zipcode,
-                                       location      : { latitude  : business.location_latitude,
-                                                         longitude : business.location_longitude } });
+const mapBusinessData = business => ({
+  countyId: business.county_number,
+  address: business.location_address,
+  licenseType: business.license_type_code,
+  name: business.business_name,
+  license: business.license_number,
+  zipCode: business.location_zipcode,
+  location: {
+    latitude: business.location_latitude,
+    longitude: business.location_longitude
+  }
+});
 
 // Array a ~> (a) => a
-const mapBusinesses   = map(mapBusinessData);
+const mapBusinesses = map(mapBusinessData);
 
 export default {
   get(params) {
-    return request.get(`${endpoint}`, { params })
+    return request.get(`${endpoint}`, {
+        params
+      })
       .then(mapBusinesses);
   }
 };
