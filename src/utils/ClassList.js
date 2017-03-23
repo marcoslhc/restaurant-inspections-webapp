18f6d7525f9abcef3b5ac3c88af6e678e67f2c89
Marcos Hernández <marcoslhc@gmail.com>
2017-03-23 13:30:53 -0400

    Refactor class list

--- src/utils/ClassList.js
+++ src/utils/ClassList.js
@@ -1,3 +1,5 @@
+const applyOverValue = functor => fn => (...args) => fn.apply(functor.join(), args);
+
 function ClassList(classes = []) {
   this._classes = classes.filter((className) => typeof className === 'string');
 }
@@ -6,8 +8,9 @@ ClassList.of = function(classes) {
   return new ClassList(classes);
 };
 
-ClassList.prototype.getString = function() {
-  return this._classes.join(' ');
+ClassList.prototype.getString = function () {
+  const join = Array.prototype.join.bind(this._classes);
+  return applyOverValue(this)(join)(' ');
 };
 
 ClassList.prototype.map = function(fn) {
@@ -15,7 +18,10 @@ ClassList.prototype.map = function(fn) {
 };
 
 ClassList.prototype.filter = function(re) {
-  return ClassList.of(this._classes.filter((className) => re.test(className)));
+  const filter = Array.prototype.filter.bind(this._classes);
+  const value = applyOverValue(this)(filter)((className) => re.test(className));
+
+  return ClassList.of(value);
 };
 
 ClassList.prototype.mapFilter = function(re, fn) {
@@ -23,7 +29,18 @@ ClassList.prototype.mapFilter = function(re, fn) {
 };
 
 ClassList.prototype.addClass = function(className) {
-  return ClassList.of(this._classes.concat([className]));
+  const concat = Array.prototype.concat.bind(this._classes);
+  const value = applyOverValue(this)(concat)([className]);
+
+  return ClassList.of(value);
+};
+
+ClassList.prototype.join = function () {
+  return this._classes;
+};
+
+ClassList.prototype.chain = function (fn) {
+  return ClassList.of(this._classes).map(fn).join();
 };
 
 ClassList.prototype.concat = ClassList.prototype.addClass;
